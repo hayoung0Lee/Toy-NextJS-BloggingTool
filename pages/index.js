c72268b31c161f474e98cbd0d16c6961c6c67ae0
hayoung0Lee <hayoung0.lee@gmail.com>
2021-03-22 12:25:52 +0900

    Add Dummy Data

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,8 @@
 import Head from "next/head";
+import dummy from "./dummy.json";
+import Card from "../components/card";
+import CardLayout from "../components/card-layout";
+import { repeatMultiple } from "../utils/common";
 
 export default function Home() {
   return (
@@ -7,7 +11,13 @@ export default function Home() {
         <title>Blog Main</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      This is home
+
+      <h3>trending</h3>
+      <CardLayout>
+        {repeatMultiple(dummy, 10).map((data, index) => {
+          return <Card key={index} data={data} />;
+        })}
+      </CardLayout>
     </>
   );
 }
