d4751fbd2c4eabfd4852ccdb913238bff17a9b73
hayoung0Lee <hayoung0.lee@gmail.com>
2021-03-23 13:25:42 +0900

    Api 추가

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -3,8 +3,38 @@
 console.log("Hello API");
 export default (req, res) => {
   // api/posts?title=1 이렇게 사용할 예정
-  const { title } = req.query;
-  console.log("title: ", title);
+  const {
+    method,
+    query: { title },
+    body,
+  } = req;
+  if (!title) {
+    switch (method) {
+      case "GET": // api/posts
+        res.status(200).json({ data: "get every post" });
+        break;
+      case "POST": // api/posts
+        // TODO json객체 이어붙이기
+        console.log("post body", body);
+        res.status(200).json({ data: `post data` });
+        break;
+      case "DELETE":
+        res.status(200).json({ data: `delete every data` });
+        break;
+      default:
+        res.setHeader("Allow", ["GET", "POST", "DELETE"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  } else {
+    switch (method) {
+      case "GET": // api/posts
+        res.status(200).json({ data: "get single post" });
+        break;
+      default:
+        res.setHeader("Allow", ["GET"]);
+        res.status(405).end(`Method ${method} Not Allowed`);
+    }
+  }
 
   // GET api/posts
 
@@ -14,5 +44,5 @@ export default (req, res) => {
 
   // DELETE api/posts : 전체 다 지우기... 과연 필요할까...
 
-  res.status(200).json({ name: "John Doe" });
+  // res.status(200).json({ name: "John Doe" });
 };
