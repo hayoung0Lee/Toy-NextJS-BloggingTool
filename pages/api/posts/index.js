f56d6400fa4c736c91f0d1dd3bf0cfad61d809d9
hayoung0Lee <hayoung0.lee@gmail.com>
2021-03-30 14:43:13 +0900

    add postgres db, connect to db, make insert and select work

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,22 +1,51 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { connectToDB } from "../../../utils/common";
+const { Client } = require("pg");
 
-console.log("Hello API");
-export default (req, res) => {
+const connect = () => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "blog",
+    password: "1234",
+    port: 5432,
+  });
+
+  client.connect();
+
+  return client;
+};
+export default async (req, res) => {
   // api/posts?title=1 이렇게 사용할 예정
+  const username = process.env.NEXT_PUBLIC_USERNAME;
+
   const {
     method,
     query: { title },
     body,
   } = req;
+
+  const client = connect(); // FIXME db 연결 위치
+
   if (!title) {
     switch (method) {
       case "GET": // api/posts
-        res.status(200).json({ data: "get every post" });
+        const get_result = await connectToDB(
+          client,
+          "select",
+          "select contents from blog_post"
+        );
+        res.status(200).json({ data: get_result });
         break;
       case "POST": // api/posts
-        // TODO json객체 이어붙이기
-        console.log("post body", body);
-        res.status(200).json({ data: `post data` });
+        const now = new Date();
+        const post_result = await connectToDB(
+          client,
+          "insert",
+          "insert into blog_post(created,modified,username,contents) values($1, $2, $3, $4) returning *",
+          [now, now, username, body.contents]
+        );
+        res.status(200).json({ data: post_result });
         break;
       case "DELETE":
         res.status(200).json({ data: `delete every data` });
