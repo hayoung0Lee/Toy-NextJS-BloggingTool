acf54c23f1d4313d23c95cb50a8802026424d697
hayoung0Lee <hayoung0.lee@gmail.com>
2021-03-30 14:44:19 +0900

    temp

--- pages/api/posts/index.js
+++ pages/api/posts/index.js
@@ -1,21 +1,39 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import { createContext } from "react";
+// import { connectToDB } from "../../../utils/common";
 
 console.log("Hello API");
-export default (req, res) => {
+
+export default async (req, res) => {
   // api/posts?title=1 이렇게 사용할 예정
+  const username = process.env.NEXT_PUBLIC_USERNAME;
+
   const {
     method,
     query: { title },
     body,
   } = req;
+
   if (!title) {
     switch (method) {
       case "GET": // api/posts
+        // const result = await connectToDB(
+        //   "select",
+        //   "select contents from blog_post"
+        // );
+        // console.log("result", result);
         res.status(200).json({ data: "get every post" });
         break;
       case "POST": // api/posts
         // TODO json객체 이어붙이기
-        console.log("post body", body);
+        // console.log("post body", body);
+        const now = new Date();
+        // const result = await connectToDB(
+        //   "insert",
+        //   "insert into blog_post(created,modified,username,contents) values($1, $2, $3, $4) returning *",
+        //   [now, now, username, body.contents]
+        // );
+        console.log("result", result);
         res.status(200).json({ data: `post data` });
         break;
       case "DELETE":
--- utils/common.js
+++ utils/common.js
@@ -1,3 +1,5 @@
+const { Client } = require("pg");
+
 const repeatMultiple = (data, index) => {
   const result = [];
 
@@ -10,4 +12,49 @@ const repeatMultiple = (data, index) => {
   return result;
 };
 
-export { repeatMultiple };
+const select = (client, text) => {
+  if (!q) return;
+  client
+    .query(q)
+    .then((res) => {
+      // console.log(res.rows[0])
+      return res.rows;
+    })
+    .catch((e) => console.error(e.stack));
+};
+
+const insert = (client, text, values) => {
+  // const text = 'INSERT INTO users(name, email) VALUES($1, $2) RETURNING *'
+  // const values = ["brianc", "brian.m.carlson@gmail.com"];
+  client
+    .query(text, values)
+    .then((res) => {
+      return res.rows;
+      // { name: 'brianc', email: 'brian.m.carlson@gmail.com' }
+    })
+    .catch((e) => console.error(e.stack));
+};
+
+const connectToDB = async (command, text, values) => {
+  const client = new Client({
+    user: "postgres",
+    host: "localhost",
+    database: "blog",
+    password: "1234",
+    port: 5432,
+  });
+
+  client.connect();
+
+  switch (command) {
+    case "select":
+      return select(client, text);
+    case "insert":
+      return insert(client, text, values);
+
+    default:
+      console.log("invalid command");
+  }
+};
+
+export { repeatMultiple, connectToDB };
