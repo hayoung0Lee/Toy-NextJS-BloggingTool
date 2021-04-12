74e38bfa03316c7ff600e1d784047ff03981217b
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-12 16:49:24 +0900

    Add link to Read Page

--- components/Card.tsx
+++ components/Card.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 import styles from "./Card.module.css";
 import { PostType } from "../utils/types";
+import Link from "next/link";
 
 interface Props {
   d: PostType;
 }
 
 const CardViewer: React.FC<Props> = ({ d }) => {
-  return <div className={styles.card}>{d.id}</div>;
+  return (
+    // TODO: hayoung -> username
+    <Link href={`/hayoung/${d.id}`}>
+      <div className={styles.card}>{d.id}</div>
+    </Link>
+  );
 };
 
 export default CardViewer;
--- pages/[username]/[id].tsx
+++ pages/[username]/[id].tsx
@@ -1,14 +1,20 @@
 import Head from "next/head";
 import React from "react";
+import { useRouter } from "next/router";
 
 const UserPost: React.FC = () => {
+  const router = useRouter();
+
   return (
     <div>
       <Head>
         <title>UserPost</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>UserPost</div>
+      <div>
+        {router.query.username}'s Post: {router.query.id}
+      </div>
+      <div>Contents for Post: {router.query.id}</div>
     </div>
   );
 };
