cfabef880a6ca0d483a14c07ef91f40232e22887
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-13 13:01:56 +0900

    Make Login API with JSON data

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,6 +1,25 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
+import testDB from "../../utils/testDB.json";
 
 export default (req: NextApiRequest, res: NextApiResponse) => {
-  res.status(200).json({ name: "Hayoung Lee" });
+  if (req.method !== "POST") {
+    res
+      .status(500)
+      .json({ message: "Sorry, login api only accepts POST method" });
+    return;
+  }
+
+  const reqJson = JSON.parse(req.body); // req -> json
+
+  if (
+    testDB.user[reqJson.id] &&
+    testDB.user[reqJson.id].password === reqJson.password
+  ) {
+    res.status(200).json({ message: "Login Success" });
+  } else {
+    res.status(200).json({ message: "Login Failed" });
+  }
 };
+
+// const r1 = await fetch("http://localhost:3000/api/login", {method: 'POST', body: JSON.stringify({id: 'hayoung', password: 1234})})
