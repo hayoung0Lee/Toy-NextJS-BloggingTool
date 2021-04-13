73ff78c79215f2fee547364ae295ad239fe76609
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-13 21:06:50 +0900

    Incremental Static Regeneration with getStaticPaths

--- pages/[username]/index.tsx
+++ pages/[username]/index.tsx
@@ -1,7 +1,38 @@
 import Head from "next/head";
 import React from "react";
+import { PostType } from "../../utils/types";
+import { GetStaticProps, GetStaticPaths } from "next";
+import { openJsonFile } from "../../utils/common";
 
-const UserHome: React.FC = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
+  console.log("getStaticPaths");
+  return {
+    paths: [
+      { params: { username: "hayoung" } }, // See the "paths" section below
+    ],
+    fallback: true, // See the "fallback" section below
+  };
+};
+
+// Incremental Static Site Regeneration
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const jsonData = await openJsonFile();
+  const username = params.username as string;
+  const data: PostType[] = jsonData.contents[username] || [];
+
+  return {
+    props: {
+      data,
+    }, // will be passed to the page component as props
+    revalidate: 1,
+  };
+};
+
+interface Props {
+  data: PostType[];
+}
+
+const UserHome: React.FC<Props> = ({ data }) => {
   return (
     <div>
       <Head>
@@ -9,6 +40,16 @@ const UserHome: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>UserHome</div>
+      <div>
+        {data?.map((d, index: number) => {
+          return (
+            <div key={index}>
+              <h2>{d.title}</h2>
+              <h5>{d.contents}</h5>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
