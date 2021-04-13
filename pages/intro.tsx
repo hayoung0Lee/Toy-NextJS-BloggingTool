df1a56a0f63b1473244bf0318a64e65872bb3105
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-13 13:50:12 +0900

    Intro page with Static Generation(getStaticProps)

--- pages/intro.tsx
+++ pages/intro.tsx
@@ -1,8 +1,25 @@
 import Head from "next/head";
 import React from "react";
+import { GetStaticProps } from "next";
+import testDB from "../utils/testDB.json";
+import { IntroType } from "../utils/types";
+
+export const getStaticProps: GetStaticProps = async (context) => {
+  const introData: IntroType[] = testDB["intro"];
+
+  return {
+    props: {
+      introData,
+    }, // will be passed to the page component as props
+  };
+};
+
+interface Props {
+  introData: IntroType[];
+}
 
 // Static Generation
-const Intro: React.FC = () => {
+const Intro: React.FC<Props> = ({ introData }) => {
   return (
     <div>
       <Head>
@@ -10,6 +27,14 @@ const Intro: React.FC = () => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <h1>Introducting ToyBlog</h1>
+      {introData.map((d, index) => {
+        return (
+          <div key={index}>
+            <h2>{d.question}</h2>
+            <h5>{d.answer}</h5>
+          </div>
+        );
+      })}
     </div>
   );
 };
