468f683eb8fb5fffb3a7be9605bae9b14dad9dd9
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-13 23:50:52 +0900

    plain json import -> json file read

--- pages/api/login.tsx
+++ pages/api/login.tsx
@@ -1,8 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from "next";
-import testDB from "../../utils/testDB.json";
+import { openJsonFile } from "../../utils/common";
 
-export default (req: NextApiRequest, res: NextApiResponse) => {
+export default async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "POST") {
     res
       .status(500)
@@ -11,10 +11,10 @@ export default (req: NextApiRequest, res: NextApiResponse) => {
   }
 
   const reqJson = JSON.parse(req.body); // req -> json
-
+  const jsonData = await openJsonFile();
   if (
-    testDB.user[reqJson.id] &&
-    testDB.user[reqJson.id].password === reqJson.password
+    jsonData.user[reqJson.id] &&
+    jsonData.user[reqJson.id].password === reqJson.password
   ) {
     res.status(200).json({ message: "Login Success" });
   } else {
