cd42972a76ff0169de20e881f95bf0061d1cffea
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-18 21:36:04 +0900

    dynamic import

--- pages/[username]/write.tsx
+++ pages/[username]/write.tsx
@@ -1,14 +1,32 @@
 import Head from "next/head";
-import React from "react";
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+
+const DynamicComponentWithNoSSR = dynamic(
+  () => import("hayoung-markdown").then((mod) => mod.App),
+  {
+    ssr: false,
+  }
+);
 
 const UserWrite: React.FC = () => {
+  const [contents, setContents] = useState<string>("");
+
   return (
     <div>
       <Head>
         <title>UserWrite</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <div>UserWrite</div>
+      {process.browser ? (
+        // <App passedContents={contents} passedSetContents={setContents} />
+        <DynamicComponentWithNoSSR
+          passedContents={contents}
+          passedSetContents={setContents}
+        />
+      ) : (
+        <div></div>
+      )}
     </div>
   );
 };
