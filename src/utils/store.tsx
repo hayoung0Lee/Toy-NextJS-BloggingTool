583b88cade770ffbc11b5a1d5a41518356de3f1c
hayoung0Lee <hayoung0.lee@gmail.com>
2021-04-21 20:27:45 +0900

    add debounceToggleDropDown func

--- src/utils/store.tsx
+++ src/utils/store.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-
+import { debounce } from "./debounce";
 // https://reactjs.org/docs/context.html
 // https://hmh.engineering/using-react-contextapi-usereducer-as-a-replacement-of-redux-as-a-state-management-architecture-336452b2930e
 const Store: any = React.createContext(null);
@@ -11,8 +11,22 @@ export const StoreProvider = ({ children }) => {
   const [isOpen, toggleDropDown] = useState<boolean>(false);
   const [reset, setReset] = useState<boolean>(false);
 
+  const handleToggle = (value: boolean) => {
+    toggleDropDown(value);
+  };
+
+  const debounceToggleDropDown = debounce(handleToggle, 200);
+
   return (
-    <Store.Provider value={{ isOpen, toggleDropDown, reset, setReset }}>
+    <Store.Provider
+      value={{
+        isOpen,
+        debounceToggleDropDown,
+        toggleDropDown,
+        reset,
+        setReset,
+      }}
+    >
       {children}
     </Store.Provider>
   );
